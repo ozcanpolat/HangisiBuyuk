72d21faeb47f2029d39e8ea0488d4fd8102482d5
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-06 13:51:29 +0300

    HangisiBuyuk

---
+++ HangisiBuyuk/HangisiBuyuk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HangisiBuyuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen 2 sayıdan hangisi büyüktür?
+
+            Console.Write("Birinci sayıyı giriniz :");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı giriniz :");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+            {
+                Console.WriteLine("Girilen birinci sayı daha büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen ikinci sayı daha büyüktür. ");
+            }
+        }
+    }
+}
